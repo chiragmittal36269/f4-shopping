c2bc197ac6ce586bfecabc92db6e7eb00d4520a0
Chirag Mittal <chiragmittal36269@gmail.com>
2023-10-11 14:49:55 +0530

    pointer added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,12 +8,24 @@ const Navbar = () => {
         <div>
             <div className="navbar">
                 <div className="left">
-                    <h3 onClick={() => navigate("/")}>Shopping Cart</h3>
+                    <h3
+                        onClick={() => navigate("/")}
+                        style={{ cursor: "pointer" }}>
+                        Shopping Cart
+                    </h3>
                 </div>
 
                 <div className="right">
-                    <h4 onClick={() => navigate("/")}>Home Page</h4>
-                    <h4 onClick={() => navigate("/cart")}>Cart Page</h4>
+                    <h4
+                        onClick={() => navigate("/")}
+                        style={{ cursor: "pointer" }}>
+                        Home Page
+                    </h4>
+                    <h4
+                        onClick={() => navigate("/cart")}
+                        style={{ cursor: "pointer" }}>
+                        Cart Page
+                    </h4>
                 </div>
             </div>
         </div>
