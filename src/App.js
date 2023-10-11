7c964e3402de430b93135cf6988c28a94f0ceb77
Chirag Mittal <chiragmittal36269@gmail.com>
2023-10-11 14:45:34 +0530

    app.js change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
         <div>
             <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="" element={<Home />} />
                 <Route path="/cart" element={<Cart />} />
             </Routes>
         </div>
